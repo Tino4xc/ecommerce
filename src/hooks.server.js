00035b805cca0c2c24342cac680bd3ec6b8d5af8
Piero Tinoco <tino4xc@gmail.com>
2023-07-19 20:41:19 -0600

    Final changes

---
+++ src/hooks.server.js
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { POSTGRES_URL } from '$env/static/private'
+process.env.POSTGRES_URL = POSTGRES_URL;
+
+export async function handle({ event, resolve }) {
+    console.log('You made a request!');
+    return await resolve(event);
+}
---
+++ src/lib/product.converter.js
@@ -0,0 +1,13 @@
+// @ts-nocheck
+export function toProduct(rows) {
+    return rows.map(row => {
+        return {
+            id: row.id,
+            name: row.name,
+            description: row.description,
+            price: row.price,
+            imageUrl: row.image_url,
+            paymentUrl: row.stripe_payment_url
+        }
+    });
+}
